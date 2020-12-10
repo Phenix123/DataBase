78e547128a24fd431dafde849fca8c7b9fd39d73
DuKeDoM <cat1020486@gmail.com>
2020-12-11 01:23:23 +0400

    Add files via upload
    Добавил запросы на обновление, удаление, добавление данных

--- testmysql.php
+++ testmysql.php
@@ -9,31 +9,43 @@ $user = 'root';
 $password = 'admin'; //To be completed if you have set a password to root
 $database = 'cup'; //To be completed to connect to a database. The database must exist.
 $port = NULL; //Default must be NULL to use default port
-$mysqli = new mysqli('127.0.0.1', $user, $password, $database, $port);
+
+$mysqli = new mysqli($host, $user, $password, $database, $port); // connect to server
 
 if ($mysqli->connect_error) {
     die('Connect Error (' . $mysqli->connect_errno . ') '
             . $mysqli->connect_error);
 }
-echo '<p>Connection OK '. $mysqli->host_info.'</p>';
-echo '<p>Server '.$mysqli->server_info.'</p>';
-echo '<p>Initial charset: '.$mysqli->character_set_name().'</p>';
+#echo '<p>Connection OK '. $mysqli->host_info.'</p>';
+#echo '<p>Server '.$mysqli->server_info.'</p>';
+#echo '<p>Initial charset: '.$mysqli->character_set_name().'</p>';
   
-$link = mysqli_connect($host, $user, $password, $database);
+$link = mysqli_connect($host, $user, $password, $database); // connect to db
 
-mysqli_set_charset($link, "utf8");
+mysqli_set_charset($link, "utf8"); // change charset to utf-8
 
-$query ="SELECT * FROM players";
-$query2 = "SELECT * FROM statistics WHERE Gameid = 1";
+$select_teams_query ="SELECT * FROM teams";
+$select_stat_query = "SELECT * FROM statistics WHERE Gameid = 1";
+$procedure_query = "call ShowsPlayersTeams(2)";
+$update_query = "UPDATE statistics SET goals = 3 WHERE GameId = 1";
+$insert_query = "INSERT INTO teams (Name, StadionId, Win, Draw, Lose) values('Команда', '17', '3', '2','3')";
+$delete_query = "DELETE from teams WHERE Name = 'Команда'";
 
-$result=mysqli_query($link, $query);
+$result4 = mysqli_query($link, $update_query);
+$result5 = mysqli_query($link, $insert_query);
+$result6 = mysqli_query($link, $delete_query);
+$result  = mysqli_query($link, $select_teams_query );
+$result2 = mysqli_query($link, $select_stat_query);
+$result3 = mysqli_query($link, $procedure_query);
 
-$result2=mysqli_query($link, $query2);
+
+if($result6)
+    printf("Удаление и добавление успешны");
 
 if($result)
 {
     $rows = mysqli_num_rows($result); // количество полученных строк
-    echo "<table><tr><th>Id</th><th>Имя Фамилия</th></tr>";
+    echo "<table><tr><th>Id</th><th>Название команды</th></tr>";
     for ($i = 0 ; $i < $rows ; ++$i)
     {
         $row = mysqli_fetch_row($result);
@@ -61,6 +73,24 @@ if($result2)
      
     // очищаем результат
     mysqli_free_result($result2);
+}
+
+if($result3)
+{
+    $rows = mysqli_num_rows($result3); // количество полученных строк
+    echo "Показать игроков 2-ой команды";
+    echo "<table><tr><th>FIO</th><th>TeamId</th></tr>";
+    for ($i = 0 ; $i < $rows ; ++$i)
+    {
+        $row = mysqli_fetch_row($result3);
+        echo "<tr>";
+            for ($j = 0 ; $j < 2 ; ++$j) echo "<td>$row[$j]</td>";
+        echo "</tr>";
+    }
+    echo "</table>";
+     
+    // очищаем результат
+    mysqli_free_result($result3);
 }
 $mysqli->close();
 ?>
